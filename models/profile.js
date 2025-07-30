9dabcb7aa59e021c06923d8e546c9e121acf1c2f
Dew's Lab🕵🏻 <macbookpro@Dews-Mac.local>
2025-07-30 23:47:20 +0700

    Setup Completed!

---
+++ models/profile.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Profile extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Profile.belongsTo(User);
+    }
+  }
+  Profile.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    photoUrl: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Profile',
+  });
+  return Profile;
+};
